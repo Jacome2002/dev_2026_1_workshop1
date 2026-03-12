9237dfe11c94dfca97f710cc906dc470f55fe905
Jacome2002 <juandajabe@hotmail.com>
2026-03-12 14:53:13 -0500

    error

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -33,7 +33,7 @@ class Conversion:
             fahrenheit_a_celsius(32) -> 0.0
             fahrenheit_a_celsius(212) -> 100.0
         """
-        return (celsius * 9/5) + 32
+        return (fahrenheit - 32) * 5/9
     
     def metros_a_pies(self, metros):
         """
